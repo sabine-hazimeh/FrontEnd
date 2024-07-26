8d97a2672e55d78adae6a112eb3a909f3150997f
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-07-27 01:29:09 +0300

    adding landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef } from "react";
+import "./style.css";
+
+import moon from "../../images/moon.png";
+import stars from "../../images/stars.png";
+import mountainsBehind from "../../images/mountains_behind.png";
+import mountainsFront from "../../images/mountains_front.png";
+
+const Home = () => {
+  const starsRef = useRef(null);
+  const moonRef = useRef(null);
+  const mountainsFrontRef = useRef(null);
+  const mountainsBehindRef = useRef(null);
+  const textRef = useRef(null);
+  const btnRef = useRef(null);
+  const headerRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const value = window.scrollY;
+
+      if (starsRef.current) starsRef.current.style.left = value * 0.25 + "px";
+      if (moonRef.current) moonRef.current.style.top = value * 1.05 + "px";
+      if (mountainsBehindRef.current)
+        mountainsBehindRef.current.style.top = value * 0.5 + "px";
+      if (mountainsFrontRef.current)
+        mountainsFrontRef.current.style.top = value * 0 + "px";
+      if (textRef.current) {
+        textRef.current.style.marginRight = value * 4 + "px";
+        textRef.current.style.marginTop = value * 1.5 + "px";
+      }
+      if (btnRef.current) btnRef.current.style.marginTop = value * 1.5 + "px";
+      if (headerRef.current) headerRef.current.style.top = value * 0.5 + "px";
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div>
+      <header ref={headerRef}>
+        <a href="#" className="logo">
+          SyntaxStudio
+        </a>
+        <ul>
+          <li>
+            <a className="active" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">SignUp</a>
+          </li>
+        </ul>
+      </header>
+
+      <section>
+        <img src={stars} id="stars" ref={starsRef} alt="stars" />
+        <img src={moon} id="moon" ref={moonRef} alt="moon" />
+        <img
+          src={mountainsBehind}
+          id="mountains_behind"
+          ref={mountainsBehindRef}
+          alt="mountains behind"
+        />
+        <h2 id="text" ref={textRef}>
+          SyntaxStudio
+        </h2>
+        <a href="#sec" id="btn" ref={btnRef}>
+          Get Started!
+        </a>
+        <img
+          src={mountainsFront}
+          id="mountains_front"
+          ref={mountainsFrontRef}
+          alt="mountains front"
+        />
+      </section>
+
+      <div className="sec" id="sec"></div>
+    </div>
+  );
+};
+
+export default Home;
