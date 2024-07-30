79209988dcf68f76f406e4d05b630c55d77dd395
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-07-31 01:49:05 +0300

    adding import users for admin

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Papa from "papaparse";
+import * as XLSX from "xlsx";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./style.css";
 
 function Admin() {
   const [users, setUsers] = useState([]);
+  const [file, setFile] = useState(null);
+  const [fileKey, setFileKey] = useState(Date.now());
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -24,11 +30,79 @@ function Admin() {
     fetchUsers();
   }, []);
 
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    // toast.success("Users have been inserted successfully!");
+    if (file) {
+      const fileType = file.name.split(".").pop();
+      let data = [];
+
+      if (fileType === "csv") {
+        Papa.parse(file, {
+          complete: (result) => {
+            if (result.data.length > 0 && typeof result.data[0] === "object") {
+              data = result.data;
+              processRows(data);
+            } else {
+              console.error("CSV parsing error: Unexpected data format");
+            }
+          },
+          header: true,
+        });
+      } else if (fileType === "xlsx") {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+          const workbook = XLSX.read(e.target.result, { type: "array" });
+          const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+          data = XLSX.utils.sheet_to_json(worksheet);
+          processRows(data);
+        };
+        reader.readAsArrayBuffer(file);
+      }
+
+      setFile(null);
+      setFileKey(Date.now());
+    }
+  };
+
+  const processRows = async (rows) => {
+    try {
+      for (let row of rows) {
+        const { name, email, password } = row;
+        await axios.post("http://127.0.0.1:8000/api/register", {
+          name,
+          email,
+          password,
+        });
+      }
+    } catch (error) {
+      console.error("Error importing user:", error);
+    }
+  };
+
   return (
     <>
       <div className="admin-page">
         <div className="users-container">
-          <button> Add new Users</button>
+          <input
+            key={fileKey}
+            type="file"
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+          />
+          <button
+            className="file-button"
+            onClick={() => document.querySelector('input[type="file"]').click()}
+          >
+            Choose File
+          </button>
+          <button className="button" onClick={handleFileUpload}>
+            Upload Users
+          </button>
+
           <ul>
             {users.map((user) => (
               <li className="users" key={user.id}>
@@ -39,6 +113,7 @@ function Admin() {
           </ul>
         </div>
       </div>
+      <ToastContainer />
     </>
   );
 }
